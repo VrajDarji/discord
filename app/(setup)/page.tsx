457128b7e34f1671f0b2cf78655a211b2c4c34cb
VrajDarji <vrajdarji573@gmail.com>
2023-12-14 00:44:46 +0530

    server creation and uplaodthing

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { intialProfile } from "@/lib/intial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import IntialModal from "@/components/modals/intial-modal";
+
+const page = async () => {
+  const profile = await intialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+  return (
+    <div>
+      <IntialModal />
+    </div>
+  );
+};
+
+export default page;
